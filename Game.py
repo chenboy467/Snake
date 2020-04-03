7327aa22974aa0fef6b0a3a3092c37876570b787
chenboy467 <andychen6788@gmail.com>
2020-04-02 17:01:35 -0700

    Create Game GUI and Snake
    Added the field and the snake to play with, made basic movements and turning for the snake.

--- Game.py
+++ Game.py
@@ -1,13 +1,83 @@
 from tkinter import *
 import random
 import functools
+import time
 
 
 class Game:
-    def __init__(self, window, size, mode):
+
+    rows = 15
+    cols = 15
+
+    snake_length = 4
+    snake_pos = [7, 3]
+    directions = {
+        'n': (0, -1),
+        'e': (1, 0),
+        's': (0, 1),
+        'w': (-1, 0)
+    }
+    direction = 'e'
+    prev_direction = direction
+
+    tiles = []
+    apple_locations = set()
+
+    def __init__(self, window, speed, mode):
         self.window = window
-        self.window.geometry('450x300')
-        self.window.resizable(False, False)
         self.window.title('Snake')
-        self.size = size
+        self.window.geometry("1000x666".format(
+            self.window.winfo_screenwidth(), self.window.winfo_screenheight()))
+        self.window.resizable(False, False)
+        self.window.bind('<Escape>', self.toggle_fullscreen)
+        self.canvas = Canvas(window)
+        self.canvas.place(relx=0.4, rely=0.5, anchor=CENTER)
+        self.speed = speed
         self.mode = mode
+
+        self.create_tiles()
+        self.move()
+        self.window.bind('<Up>', functools.partial(self.turn, 'n'))
+        self.window.bind('<Right>', functools.partial(self.turn, 'e'))
+        self.window.bind('<Down>', functools.partial(self.turn, 's'))
+        self.window.bind('<Left>', functools.partial(self.turn, 'w'))
+
+    def toggle_fullscreen(self, event):
+        self.window.attributes(
+            '-fullscreen', not self.window.attributes('-fullscreen'))
+
+    def create_tiles(self):
+        self.tile_values = [
+            [0 for x in range(self.cols)] for y in range(self.rows)]
+        self.tiles = [[0 for x in range(self.cols)]
+                      for y in range(self.rows)]
+        for x in range(self.rows):
+            for y in range(self.cols):
+                self.tiles[x][y] = Frame(
+                    self.canvas, bg=('grey', 'white')[(x+y) % 2], height=40, width=40)
+                self.tiles[x][y].grid(
+                    row=x, column=y, sticky='ew')
+
+    def move(self):
+        self.prev_direction = self.direction
+        for x in range(self.rows):
+            for y in range(self.cols):
+                # self.tiles[x][y].configure(bg='green')
+                self.tile_values[x][y] -= (self.tile_values[x][y] > 0)
+                if self.tile_values[x][y]:
+                    self.tiles[x][y].configure(bg='green3')
+                else:
+                    self.tiles[x][y].configure(
+                        bg=('gray85', 'white')[(x+y) % 2])
+        self.snake_pos = [self.snake_pos[0] + self.directions[self.direction][1], self.snake_pos[1] +
+                          self.directions[self.direction][0]]
+
+        self.tile_values[self.snake_pos[0]
+                         ][self.snake_pos[1]] = self.snake_length
+        self.tiles[self.snake_pos[0]][self.snake_pos[1]].configure(bg='green4')
+        self.window.after(400 - self.speed*100, self.move)
+
+    def turn(self, direction, event):
+        if (self.prev_direction == 'n' and direction == 's') or (self.prev_direction == 'e' and direction == 'w') or (self.prev_direction == 's' and direction == 'n') or (self.prev_direction == 'w' and direction == 'e'):
+            return
+        self.direction = direction
--- Snake.py
+++ Snake.py
@@ -7,18 +7,19 @@ import Game
 class Launcher:
     def __init__(self, window):
         self.window = window
-        self.window.geometry('450x300')
-        self.window.resizable(False, False)
         self.window.title('Launcher')
+        self.window.geometry('450x250')
+        self.window.resizable(False, False)
 
-        Label(window, text='Snake', font='Arial 48 bold').grid(
-            row=0, column=0, columnspan=3)
+        self.title = Label(window, text='Snake', font='Arial 48 bold')
+        self.title.grid(row=0, columnspan=4, pady=10)
+
         for x in range(3):
-            Button(window, text=('SLOW', 'MEDIUM', 'LARGE')[
-                   x], command=functools.partial(self.start, x)).grid(row=1, column=x)
+            Button(window, text=('SLOW', 'MEDIUM', 'FAST')[
+                   x], font='Ariel 18 bold', command=functools.partial(self.start, x), width=8).grid(row=1, column=x, columnspan=1, padx=10, pady=3)
         self.mode_select = Spinbox(window, values=(
-            'Rabbits', 'Borderless', 'Bombs', 'Multifruit', 'Standard'))
-        self.mode_select.grid(row=2, column=1)
+            'Rabbits', 'Borderless', 'Bombs', 'Multifruit', 'Standard'), width=8)
+        self.mode_select.grid(row=2, column=1, columnspan=1, pady=20)
 
     def start(self, speed):
         root = Tk()
