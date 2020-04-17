94c7ff3fd0802745d95b34003b2411d4b1d05731
chenboy467 <andychen6788@gmail.com>
2020-04-16 17:01:18 -0700

    Create Rabbit AI and Skins
    Created the rabbit AI to move randomly and attempt to avoid the player. Also changed mode spinbox into an option menu. Started making skins for the snake.

--- Game.py
+++ Game.py
@@ -22,11 +22,13 @@ class Game:
 
     tiles = []
     fruit_amount = 5
+    rabbit_amount = 3
+    rabbit_pos = []
     bomb_amount = 3
 
     game_paused = True
 
-    def __init__(self, window, speed, mode):
+    def __init__(self, window, speed, mode, skin):
         self.window = window
         self.window.title('Snake')
         self.window.geometry("1000x666".format(
@@ -37,15 +39,20 @@ class Game:
         self.canvas.place(relx=0.4, rely=0.5, anchor=CENTER)
         self.speed = speed
         self.mode = mode
+        self.skin = skin
 
         self.create_tiles()
-        self.spawn_item(-1)
-        if self.mode == 'Multifruit':
-            for x in range(self.fruit_amount - 1):
-                self.spawn_item(-1)
-        if self.mode == 'Bombs':
-            for x in range(self.bomb_amount):
-                self.spawn_item(-3)
+        if self.mode == 'Rabbits':
+            for x in range(self.rabbit_amount):
+                self.spawn_item(-2)
+        else:
+            self.spawn_item(-1)
+            if self.mode == 'Multifruit':
+                for x in range(self.fruit_amount - 1):
+                    self.spawn_item(-1)
+            if self.mode == 'Bombs':
+                for x in range(self.bomb_amount):
+                    self.spawn_item(-3)
         self.window.bind('<Up>', functools.partial(self.turn, 'n'))
         self.window.bind('<Right>', functools.partial(self.turn, 'e'))
         self.window.bind('<Down>', functools.partial(self.turn, 's'))
@@ -61,7 +68,8 @@ class Game:
         Button(window, text='RESET', font='Arial 18 bold',
                command=self.reset, width=8).place(relx=0.75, rely=0.9, anchor=SW)
 
-        self.tiles[self.snake_pos[0]][self.snake_pos[1]].configure(bg='green4')
+        self.tiles[self.snake_pos[0]][self.snake_pos[1]].configure(bg=(
+            'red4', 'DarkOrange4', 'yellow4', 'green4', 'DodgerBlue4', 'DarkOrchid4', 'sienna4', 'grey30', 'red', (('gray85', 'white')[(self.snake_pos[0]+self.snake_pos[1]) % 2]))[self.skin])
 
     def timer(self):
         current_time = time.time()
@@ -69,7 +77,8 @@ class Game:
         minutes = int(delta_time//60)
         seconds = int(delta_time % 60)
         if delta_time < 3600 and not self.game_paused:
-            self.clock.config(text='{0:02d}:{1:02d}'.format(minutes, seconds))
+            self.clock.config(
+                text='{0:02d}:{1:02d}'.format(minutes, seconds))
             self.window.after(100, self.timer)
 
     def toggle_fullscreen(self, event):
@@ -82,6 +91,8 @@ class Game:
         self.start_time = time.time()
         self.timer()
         self.move()
+        if self.mode == 'Rabbits':
+            self.rabbit_move()
 
     def create_tiles(self):
         self.tile_values = [
@@ -122,30 +133,38 @@ class Game:
                     (self.snake_pos[1] - self.directions[self.direction][0]) % self.col_length].configure(bg='snow4')
                 return
 
-            # running into itself
-            if self.tile_values[self.snake_pos[0]][self.snake_pos[1]] > 0:
-                self.game_paused = True
-                for x in range(self.col_length):
-                    for y in range(self.row_length):
-                        if self.tile_values[x][y] > 0:
-                            self.tiles[x][y].configure(bg='snow3')
-                        elif self.tile_values[x][y] == 0:
-                            self.tiles[x][y].configure(
-                                bg=('gray85', 'white')[(x+y) % 2])
-                self.tiles[(self.snake_pos[0] - self.directions[self.direction][1]) % self.row_length][
-                    (self.snake_pos[1] - self.directions[self.direction][0]) % self.col_length].configure(bg='snow4')
-                return
+        # running into itself
+        if self.tile_values[self.snake_pos[0]][self.snake_pos[1]] > 0:
+            self.game_paused = True
+            for x in range(self.col_length):
+                for y in range(self.row_length):
+                    if self.tile_values[x][y] > 0:
+                        self.tiles[x][y].configure(bg='snow3')
+                    elif self.tile_values[x][y] == 0:
+                        self.tiles[x][y].configure(
+                            bg=('gray85', 'white')[(x+y) % 2])
+            self.tiles[(self.snake_pos[0] - self.directions[self.direction][1]) % self.row_length][
+                (self.snake_pos[1] - self.directions[self.direction][0]) % self.col_length].configure(bg='snow4')
+            return
 
         # eating fruit
-        if self.tile_values[self.snake_pos[0]][self.snake_pos[1]] == -1:
+        if self.tile_values[self.snake_pos[0]][self.snake_pos[1]] == -1 or self.tile_values[self.snake_pos[0]][self.snake_pos[1]] == -2:
             self.snake_length += 1
-            if (self.row_length*self.col_length - self.snake_length) >= self.fruit_amount:
-                self.spawn_item(-1)
+            if self.mode == 'Rabbits':
+                for x in self.rabbit_pos:
+                    if x[0] == self.snake_pos[0] and x[1] == self.snake_pos[1]:
+                        x[0] = -1
+                        x[1] = -1
+                if (self.row_length*self.col_length - self.snake_length) >= self.rabbit_amount:
+                    self.spawn_item(-2)
+            else:
+                if (self.row_length*self.col_length - self.snake_length) >= self.fruit_amount:
+                    self.spawn_item(-1)
             for x in range(self.col_length):
                 for y in range(self.row_length):
                     if self.tile_values[x][y] > 0:
                         self.tile_values[x][y] += 1
-            if self.snake_length % 3 == 0:
+            if self.snake_length % 3 == 0 and self.mode == 'Bombs':
                 self.spawn_item(-3)
 
         # eating bomb
@@ -169,6 +188,7 @@ class Game:
                 self.tile_values[x][y] -= (self.tile_values[x][y] > 0)
                 if self.tile_values[x][y] > 0:
                     self.tiles[x][y].configure(bg='green3')
+                    # TODO: MAKE SKINS APPLY FOR WHOLE BODY
                 elif self.tile_values[x][y] == 0:
                     self.tiles[x][y].configure(
                         bg=('gray85', 'white')[(x+y) % 2])
@@ -198,6 +218,72 @@ class Game:
             return
         self.direction = direction
 
+    def rabbit_move(self):
+        if self.game_paused:
+            return
+
+        for x in self.rabbit_pos:
+            if not x[0] == -1:
+                # if abs(x[0] - self.snake_pos[0]) < 4 or abs(x[1] - self.snake_pos[1]) < 4:
+                # pass
+                # else:
+                if True:
+                    rabbit_direction = ('n', 'e', 's', 'w')[
+                        random.randint(0, 3)]
+                    if abs(x[0] - self.snake_pos[0]) < 6 or abs(x[1] - self.snake_pos[1]) < 6:
+                        if abs(x[0] - self.snake_pos[0]) > abs(x[1] - self.snake_pos[1]):
+                            if x[0] - self.snake_pos[0] < 0:
+                                rabbit_direction = 'w'
+                            elif x[0] - self.snake_pos[0] > 0:
+                                rabbit_direction = 'e'
+                        elif abs(x[0] - self.snake_pos[0]) < abs(x[1] - self.snake_pos[1]):
+                            if x[1] - self.snake_pos[1] < 0:
+                                rabbit_direction = 'n'
+                            elif x[1] - self.snake_pos[1] > 0:
+                                rabbit_direction = 's'
+                        else:
+                            if x[0] - self.snake_pos[0] < 0 and x[1] - self.snake_pos[1] < 0:
+                                rabbit_direction = ('e', 's')[
+                                    random.randint(0, 1)]
+                            elif x[0] - self.snake_pos[0] > 0 and x[1] - self.snake_pos[1] < 0:
+                                rabbit_direction = ('w', 's')[
+                                    random.randint(0, 1)]
+                            elif x[0] - self.snake_pos[0] < 0 and x[1] - self.snake_pos[1] > 0:
+                                rabbit_direction = ('e', 'n')[
+                                    random.randint(0, 1)]
+                            elif x[0] - self.snake_pos[0] > 0 and x[1] - self.snake_pos[1] > 0:
+                                rabbit_direction = ('w', 'n')[
+                                    random.randint(0, 1)]
+                    else:
+                        rabbit_direction = ('n', 'e', 's', 'w')[
+                            random.randint(0, 3)]
+                    path_clear = False
+                    while path_clear == False:
+                        path_clear = True
+                        while (x[0] + self.directions[rabbit_direction][1] < 0) or (x[0] + self.directions[rabbit_direction][1] >= self.row_length) or (x[1] + self.directions[rabbit_direction][0] < 0) or (x[1] + self.directions[rabbit_direction][0] >= self.col_length):
+                            rabbit_direction = ('n', 'e', 's', 'w')[
+                                random.randint(0, 3)]
+                            path_clear = False
+                        if (self.tile_values[x[0] + self.directions[rabbit_direction][1]][x[1] + self.directions[rabbit_direction][0]]):
+                            rabbit_direction = ('n', 'e', 's', 'w')[
+                                random.randint(0, 3)]
+                            path_clear = False
+                    self.tile_values[x[0]][x[1]] = 0
+                    self.tiles[x[0]][x[1]].configure(
+                        bg=('gray85', 'white')[(x[0]+x[1]) % 2])
+                    x[0] = x[0] + self.directions[rabbit_direction][1]
+                    x[1] = x[1] + self.directions[rabbit_direction][0]
+                    self.tile_values[x[0]][x[1]] = -2
+                    self.tiles[x[0]][x[1]].configure(bg='pink')
+
+        # rabbit body update
+        for x in range(self.col_length):
+            for y in range(self.row_length):
+                if self.tile_values[x][y] == -2:
+                    self.tiles[x][y].configure(bg='pink')
+
+        self.window.after((400 - self.speed*100) * 2, self.rabbit_move)
+
     def spawn_item(self, item_index):
         item_pos = [random.randint(
             0, self.row_length - 1), random.randint(0, self.col_length - 1)]
@@ -212,6 +298,14 @@ class Game:
         self.tile_values[item_pos[0]][item_pos[1]] = item_index
         self.tiles[item_pos[0]][item_pos[1]].configure(
             bg=(None, 'red', 'pink', 'black')[item_index * -1])
+        if item_index == -2:
+            if len(self.rabbit_pos) < self.rabbit_amount:
+                self.rabbit_pos.append([item_pos[0], item_pos[1]])
+            else:
+                for x in self.rabbit_pos:
+                    if x[0] == -1:
+                        x[0] = item_pos[0]
+                        x[1] = item_pos[1]
 
     def reset(self):
         self.snake_length = 3
@@ -221,13 +315,18 @@ class Game:
         self.tiles = []
         self.game_paused = True
         self.create_tiles()
-        self.spawn_item(-1)
-        if self.mode == 'Multifruit':
-            for x in range(self.fruit_amount - 1):
-                self.spawn_item(-1)
-        if self.mode == 'Bombs':
-            for x in range(self.bomb_amount):
-                self.spawn_item(-3)
+        self.rabbit_pos = []
+        if self.mode == 'Rabbits':
+            for x in range(self.rabbit_amount):
+                self.spawn_item(-2)
+        else:
+            self.spawn_item(-1)
+            if self.mode == 'Multifruit':
+                for x in range(self.fruit_amount - 1):
+                    self.spawn_item(-1)
+            if self.mode == 'Bombs':
+                for x in range(self.bomb_amount):
+                    self.spawn_item(-3)
         self.tiles[self.snake_pos[0]][self.snake_pos[1]].configure(bg='green4')
         self.start_time = time.time()
         self.clock.config(text='00:00')
--- Snake.py
+++ Snake.py
@@ -17,13 +17,28 @@ class Launcher:
         for x in range(3):
             Button(window, text=('SLOW', 'MEDIUM', 'FAST')[
                    x], font='Ariel 18 bold', command=functools.partial(self.start, x), width=8).grid(row=1, column=x, columnspan=1, padx=10, pady=3)
-        self.mode_select = Spinbox(window, values=(
-            'Rabbits', 'Borderless', 'Bombs', 'Multifruit', 'Standard'), width=8)
+        # self.mode_select = Spinbox(window, values=(
+        #    'Rabbits', 'Borderless', 'Bombs', 'Multifruit', 'Standard'), width=8)
+        self.modes = ['Standard', 'Borderless',
+                      'Multifruit', 'Bombs', 'Rabbits']
+        self.selected_mode = StringVar()
+        self.selected_mode.set(self.modes[0])
+        self.mode_select = OptionMenu(
+            window, self.selected_mode, *self.modes)
         self.mode_select.grid(row=2, column=1, columnspan=1, pady=20)
+
+        self.skins = ['Red', 'Orange', 'Yellow', 'Green', 'Blue',
+                      'Purple', 'Brown', 'Gray', 'Rainbow', 'Invisible']
+        self.selected_skin = StringVar()
+        self.selected_skin.set(self.skins[3])
+        self.skin_select = OptionMenu(
+            window, self.selected_skin, *self.skins)
+        self.skin_select.grid(row=2, column=0, columnspan=1, pady=20)
 
     def start(self, speed):
         root = Tk()
-        game_gui = Game.Game(root, speed, self.mode_select.get())
+        game_gui = Game.Game(
+            root, speed, self.selected_mode.get(), self.skins.index(self.selected_skin.get()))
         root.mainloop()
 
 
